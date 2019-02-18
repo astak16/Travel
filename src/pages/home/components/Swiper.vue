2eb31037a0a4d59ca4a55ba0cd3c70f56cd6a430
tiantian <1500846601@qq.com>
2019-02-18 19:51:06 +0800

    add index-ajax

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="wrapper">
-      <swiper :options="swiperOption">
+      <swiper :options="swiperOption" v-if="showSwiperList">
           <swiper-slide v-for="item of swiperList" :key="item.id" alt="">
             <img class="swiper-img" :src="item.imgUrl">
           </swiper-slide>
@@ -12,22 +12,20 @@
 <script>
 export default {
   name: 'HomeSwiper',
+  props: {
+    swiperList: Array
+  },
   data () {
     return {
       swiperOption: {
         pagination: '.swiper-pagination',
         loop: true
-      },
-      swiperList: [{
-        id: '0001',
-        imgUrl: '//i8.mifile.cn/v1/a1/a40dde16-6796-fc8d-2fb4-4983106ab8fb!720x360.webp'
-      }, {
-        id: '0002',
-        imgUrl: '//i8.mifile.cn/v1/a1/f91bc067-b5cb-02da-e0a8-cc3fe812b76b!720x360.webp'
-      }, {
-        id: '0003',
-        imgUrl: '//i8.mifile.cn/v1/a1/3bd24537-a0f9-58d7-d468-e158b67af652!720x360.webp'
-      }]
+      }
+    }
+  },
+  computed: {
+    showSwiperList () {
+      return this.swiperList.length
     }
   }
 }
--- src/pages/home/components/Weekend.vue
+++ src/pages/home/components/Weekend.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="weekend-title">周末去哪儿</div>
     <ul>
-      <li class="item" v-for="item of recommendList" :key="item.id">
+      <li class="item" v-for="item of weekendList" :key="item.id" v-if="showWeekendList">
         <div class="item-img-wrapper">
           <img class="item-img" :src="item.imgUrl" alt="">
         </div>
@@ -18,24 +18,12 @@
 <script>
 export default {
   name: 'HomeWeekend',
-  data () {
-    return {
-      recommendList: [{
-        id: '0001',
-        imgUrl: 'http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg',
-        title: '京城有好泉',
-        desc: '细数北京温泉，温暖你的冬天'
-      }, {
-        id: '0002',
-        imgUrl: 'http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg',
-        title: '京城有好泉',
-        desc: '细数北京温泉，温暖你的冬天'
-      }, {
-        id: '0003',
-        imgUrl: 'http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg',
-        title: '京城有好泉',
-        desc: '细数北京温泉，温暖你的冬天'
-      }]
+  props: {
+    weekendList: Array
+  },
+  computed: {
+    showWeekendList () {
+      return this.weekendList.length
     }
   }
 }
@@ -44,14 +32,13 @@ export default {
 <style scoped lang="stylus">
   @import "~styles/mixins.styl"
   .weekend-title
-    margin-top: .2rem
     line-height: .8rem
     background: #eee
     text-indent: .2rem
     font-size:16px
   .item-img-wrapper
     height: 0
-    padding-bottom: 33.9%
+    padding-bottom: 37.09%
     overflow: hidden
     .item-img
       width: 100%
